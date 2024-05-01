91b2b549ec4cf004c38c4ffad6db407bd450e147
Tobi <tobiawolaju21@gmail.com>
2024-05-01 13:56:08 +0100

    hkjblwa

--- frontend/tasks.js
+++ frontend/tasks.js
@@ -6,3 +6,16 @@
      const randomX = Math.floor(Math.random() * 91) - 49; // Random value between -25 and 25
      button.style.transform = `translateX(${randomX}px)`;
  });
+
+
+ 
+//next page
+document.getElementById("play").addEventListener("click", function() {
+    // Add click animation class to the button
+    this.classList.add("click-animation");
+
+    // Redirect to home.html after the animation ends
+    setTimeout(() => {
+        window.location.href = "wrap.html";
+    }, 300); // 0.3 seconds for the animation duration
+});
