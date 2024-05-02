4b8813f07bea892ec14a95e518abff094126d73e
Tobi <tobiawolaju21@gmail.com>
2024-05-02 13:11:43 +0100

    klo;dk

--- frontend/scripts/tasks.js
+++ frontend/scripts/tasks.js
@@ -10,7 +10,18 @@
 
  
 //next page
-document.getElementById("play").addEventListener("click", function() {
+document.getElementById("play1").addEventListener("click", function() {
+    // Add click animation class to the button
+    this.classList.add("click-animation");
+
+    // Redirect to home.html after the animation ends
+    setTimeout(() => {
+        window.location.href = "wrap.html";
+    }, 300); // 0.3 seconds for the animation duration
+});
+
+//next page
+document.getElementById("play2").addEventListener("click", function() {
     // Add click animation class to the button
     this.classList.add("click-animation");
 
