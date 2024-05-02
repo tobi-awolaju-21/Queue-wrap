18052e6e713d70fe8f2503d5aea7d1308e2a30b8
Tobi <tobiawolaju21@gmail.com>
2024-05-02 01:27:24 +0100

    bjhkhldx

--- frontend/home.js
+++ frontend/home.js
@@ -21,7 +21,32 @@ if (currentUser) {
 } else {
     // User not logged in
     console.log('No user logged in');
+  
+
+}
+
+
+// Retrieve the user object from localStorage
+const storedData = localStorage.getItem('currentUser');
+
+if (storedData) {
+    const parsedData = JSON.parse(storedData);
+    const storedUser = parsedData.user;
+    const storedTimestamp = parsedData.timestamp;
+
+    const currentTime = new Date().getTime(); // Get current time in milliseconds
+    const elapsedTime = (currentTime - storedTimestamp) / 1000; // Calculate elapsed time in seconds
+
+    if (elapsedTime > 60) {
+        console.log("Stored user object is more than 60 seconds old.");
+        window.close();
+        window.location.href = "index.html";
+    } else {
+        // User object is still valid, use it
+        console.log(storedUser);
+    }
+} else {
+    console.log("No user data stored in localStorage.");
     window.close();
     window.location.href = "index.html";
-
 }
