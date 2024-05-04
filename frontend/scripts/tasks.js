d5aceba104c17c6418de20903a28a2852487e5d3
Tobi <tobiawolaju21@gmail.com>
2024-05-04 13:22:20 +0100

    jxskhslxs

--- frontend/scripts/tasks.js
+++ frontend/scripts/tasks.js
@@ -201,7 +201,14 @@ function uploadImage(file) {
                         const LatestjsonData2 = JSON.parse(LatestjsonData);
 
     const dataish = JSON.parse(LatestjsonData);
-    const img3 = dataish[key]["img3"];
+
+    
+    url1 = dataish[key]["img1"];
+    url2 = dataish[key]["img2"];
+    url3 = dataish[key]["img3"];
+    url4 = dataish[key]["img4"];
+
+
     console.log("this is it kiss it:"+ img3);
     
 
@@ -243,10 +250,10 @@ function uploadImage(file) {
                 var jsonData2 = {
                     timestamp: timestamp,
                     feeling: feeling,
-                    img1: "url1",
-                    img2: "url2",
-                    img3: "url3",
-                    img4: "url4"
+                    img1: url,
+                    img2: url2,
+                    img3: url3,
+                    img4: url4
                 };
 
 
