072259eba9dae01ea92c307ff7870ac9140269bc
Tobi <tobiawolaju21@gmail.com>
2024-05-05 20:37:56 +0100

    nbkjdxx

--- frontend/scripts/wrap.js
+++ frontend/scripts/wrap.js
@@ -1,5 +1,5 @@
 //next page
-document.getElementById("wrap2").addEventListener("click", function() {
+document.getElementById("preview").addEventListener("click", function() {
     // Add click animation class to the button
     this.classList.add("click-animation");
 
