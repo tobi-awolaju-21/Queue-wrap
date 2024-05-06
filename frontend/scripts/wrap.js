9196e77ca1db25f54328fcbdc3b7f4e545f9ebe6
Tobi <tobiawolaju21@gmail.com>
2024-05-06 02:46:23 +0100

    bjhsks

--- frontend/scripts/wrap.js
+++ frontend/scripts/wrap.js
@@ -88,28 +88,6 @@ storedWrap = localStorage.getItem('currentWrap');
 if (storedWrap) {
     // Parse the JSON string back to an object
     const parsedData = JSON.parse(storedWrap);
-    // Extract image URLs from the parsed object
-     feeling = parsedData.feeling;
-     src1 = parsedData.img1;
-     src2 = parsedData.img2;
-     src3 = parsedData.img3;
-     src4 = parsedData.img4;
-     wrap = parsedData.wrap;
-
-     const imageElements1 = [
-        document.getElementById('1img'),
-        document.getElementById('2img'),
-        document.getElementById('3img'),
-        document.getElementById('4img')
-    ];
-    // Assign image sources to the corresponding image elements
-    imageElements1[0].src = src1;
-    imageElements1[1].src = src2;
-    imageElements1[2].src = src3;
-    imageElements1[3].src = src4;
-     // load on webpage
-
-
    
 } else {
     // Handle case when 'currentWrap' is not found in localStorage
